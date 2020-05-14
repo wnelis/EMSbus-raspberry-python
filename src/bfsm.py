7e2916ebb2b4769e5b83df975ab48eabe6e971c3
Wim Nelis <wim.nelis@ziggo.nl>
2020-05-14 13:15:09 +0200

    Module bfsm.py: Remove class WatchdogTimer. Allow for a parameter to be specified with a stimulus. Require references to actions to be 'bound object' references in case this FSM is embedded in an object.

--- src/bfsm.py
+++ src/bfsm.py
@@ -18,69 +18,21 @@
 # event action will be invoked too, but if it returns a False value, the event
 # action will be skipped.
 #
-# For convenience a single watchdog-timer, named wdt, is created together with
-# the FSM. It is not used within this class.
+# If this FSM will be part of an object, in which case the actions will be
+# methods of the embedding object, the references to the actions should be
+# 'bound method references'. This can be enforced using function
+# types.MethodType.
 #
 # Written by W.J.M. Nelis, wim.nelis@ziggo.nl, 2018.01
 #
 import queue
 import time
-from threading import Timer
-
-#
-# Define a simple watchdog timer. A specific exception subclass is defined to
-# handle a time-out for which no handler is defined.
-#
-class BfsmTimeoutException( Exception ):
-  '''An unhandled time-out at BFSM.'''
-
-class WatchdogTimer:
-  '''A simple, stoppable and restartable watchdog timer.'''
-  def __init__( self ):
-    self.timeout= None			# Time out value [s]
-    self.handler= None			# Call back function, parameterless
-    self.timer  = None			# Timer object instance
-
-  def _handler( self ):			# Default time-out handler
-    raise BfsmTimeoutException
-
- # Method reset stops the timer if it is running, and creates and starts a new
- # one using the parameters passed to the previous invokation of method start.
-  def reset( self ):			# Reset a running timer
-    if self.timer is None:
-      return False			# No timer defined
-    if self.timer.is_alive():
-      self.timer.cancel()
-    self.timer= Timer( self.timeout, self.handler )
-    self.timer.start()
-    return True
-
- # Method start starts a new timer. If there was a timer already running, it is
- # stopped without further notice.
-  def start( self, timeout, Handler=None ):	# Start a timer
-    self.timeout= timeout
-    self.handler= Handler	if Handler is not None else self._handler
-    if self.timer is not None  and  self.timer.is_alive():
-      self.timer.cancel()
-    self.timer= Timer( self.timeout, self.handler )
-    self.timer.start()
-
- # Method stop stops the timer if it is running. It returns true if the timer
- # is stopped, false if the timer already was expired.
-  def stop( self ):			# Stop a timer
-    if self.timer is not None  and  self.timer.is_alive():
-      self.timer.cancel()
-      return True
-    else:
-      return False
-
 
 class Bfsm:
   '''A very basic Finite State Machine'''
 
-  def __init__( self, Matrix, Owner, StaAct=None ):
+  def __init__( self, Matrix, StaAct=None ):
     self.Matrix= Matrix			# Decision table of the FSM
-    self.Owner = Owner			# Object 'owning' the actions
     self.StaAct= StaAct			# State pre- and post-action vector
 
    # Build a list of all possible states and a list of all possible stimuli, by
@@ -101,7 +53,7 @@ class Bfsm:
         assert Stim in Matrix[State]
         assert Matrix[State][Stim][0] in self.States
 
-   # If a list state actions is specified, make sure it contains an entry for
+   # If a list of state actions is specified, make sure it contains an entry for
    # each state.
     if StaAct is not None:
       for State in self.States:
@@ -117,7 +69,7 @@ class Bfsm:
     self.PrvState= None			# Previous state
     self.NxtState= None			# Next state
     self.Stimulus= None			# Event
-    self.wdt     = WatchdogTimer()	# Single timer per FSM
+    self.Parametr= None			# Event parameter
 
  # Private method _Interpret is the interpreter of the BFSM. It fetches the next
  # stimulus from either the high-prior queue or the default queue, determines
@@ -133,6 +85,13 @@ class Bfsm:
       if Stim is None:
         return
 
+      if type(Stim) is tuple:
+        (Stim,self.Parametr)= Stim[:]
+      elif type(Stim) is str:
+        self.Parametr= None
+      else:
+        assert False, 'Unexpected event type'
+
       assert self.Stimuli.count( Stim ) == 1
       (NewState,Action)= self.Matrix[self.State][Stim][:]
       assert self.States.count( NewState ) == 1
@@ -141,18 +100,25 @@ class Bfsm:
       if self.StaAct is not None:	# Do state action
         if self.StaAct[self.State] is not None:
           self.NxtState= NewState
-          if not self.StaAct[self.State]( self.Owner ):
+          if not self.StaAct[self.State]():
             continue
-      Action( self.Owner )		# Invoke event action method
+      if self.Parametr is None:
+        Action()
+      else:
+        Action( self.Parametr )
+
       self.PrvState= self.State
       self.State   = NewState
       self.NxtState= None
 
  # Method ReportEvent enters the supplied stimulus in the default queue and
  # returns. The stimulus will be handled when method HandleEvent is called.
-  def ReportEvent( self, Stim ):
+  def ReportEvent( self, Stim, Par=None ):
     assert self.Stimuli.count( Stim ) == 1
-    self.DefQueue.put( Stim )
+    if Par is None:
+      self.DefQueue.put( Stim )
+    else:
+      self.DefQueue.put( (Stim,Par) )
 
  # Method AugmentEvent enters the supplied stimulus in the high priority queue.
  # Typically, this method is called from an action invoked by this FSM, causing
@@ -163,8 +129,8 @@ class Bfsm:
 
  # Method HandleEvent enters the supplied stimulus in the default queue and
  # invokes the FSM interpreter to handle the stimulus.
-  def HandleEvent( self, Stim ):
-    self.ReportEvent( Stim )
+  def HandleEvent( self, Stim, Par=None ):
+    self.ReportEvent( Stim, Par )
     self._Interpret()
 
  # Method GetState retrieves the current state and the last stimulus. Note that
